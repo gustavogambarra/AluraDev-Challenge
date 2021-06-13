c743c75c7cef70d642fad51245a1088fc29368bf
Gustavo Gambarra <gustavo.gambarra@ccc.ufcg.edu.br>
2021-06-13 18:56:19 -0300

    botão de cor e highlight funcionando

---
+++ assets/scripts/main.js
@@ -0,0 +1,32 @@
+const codeArea = document.querySelector('.code__editor__wrapper')
+const language = document.querySelector('.language')
+const button = document.querySelector('.highlight-button')
+
+function changeLanguage() {
+    const code = codeArea.querySelector('code')
+    console.log(language.value)
+    codeArea.innerHTML = `<code class="preview hljs ${language.value}" contenteditable="true" aria-label="editor" spellcheck="false"></code>`
+    codeArea.firstChild.innerText = code.innerText
+}
+
+language.addEventListener('change', () => {
+    changeLanguage()
+})
+
+console.log(language.value)
+
+button.addEventListener('click', () => {
+    const code = codeArea.querySelector('code')
+    hljs.highlightBlock(code)
+})
+
+
+
+const selectColor = (event) => {
+    let value = event.target.value
+    const codeEditorBg = document.querySelector('.code__editor__bg')
+    return codeEditorBg.style.backgroundColor = value
+} 
+
+const color = document.querySelector('.color__selector__input')
+color.addEventListener('input', selectColor)
